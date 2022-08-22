cb845770189df3f6dcab84c6e499b67feb068c5d
Nicole <85316345+nicolemotoche29@users.noreply.github.com>
2022-08-22 08:54:04 -0500

    Update Sueldos.java

--- src/Sueldos.java
+++ src/Sueldos.java
@@ -1,26 +1,25 @@
 public class Sueldos {
 
     private float sueldo;
-    public void setSueldo(){
+
+    public void setSueldo() {
         sueldo = 375.37f;
     }
 
-<<<<<<< HEAD
-    public void setSueldo(float sueldoIngeniero){
+    public void setSueldo(float sueldoIngeniero) {
 
-        sueldoIngeniero = sueldo*3;
+        sueldoIngeniero = sueldo * 3;
         sueldo = sueldoIngeniero;
     }
 
-    float getSuedlo (){
+    float getSuedlo() {
 
-=======
-    public void setSueldo(float nuevoSueldo){
-        sueldo = nuevoSueldo;
-    }
+        public void setSueldo ( float nuevoSueldo){
+            sueldo = nuevoSueldo;
+        }
 
-    float getSueldo (){
->>>>>>> 866b8214bb9ee882e135622c8fb75b9bbbb76da1
-        return sueldo;
+        float getSueldo () {
+            return sueldo;
+        }
     }
 }
