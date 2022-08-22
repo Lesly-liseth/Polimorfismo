0653020b2151027524426272f0cb4c782523444a
Mayra Ñaupari <mayra.naupari@epn.edu.ec>
2022-08-22 09:03:28 -0500

    cambio

--- src/Principal.java
+++ src/Principal.java
@@ -1,11 +1,9 @@
 public class Principal {
 
-<<<<<<< HEAD
-=======
 
-    public static void main(String[] args) {
-        Sueldos sueldo = new Sueldos();
->>>>>>> 04cb010fa1bae61cb8859c5ff228e367db8d16f6
+
+    
+
 
     public static void main(String[] args) {
         Sueldo sueldo = new Sueldo();
