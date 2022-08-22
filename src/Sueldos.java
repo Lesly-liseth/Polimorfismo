418e7eda3e1cebff198fc3f90c2c33bdf3182a94
Lesly <leslyherrera851@gmail.com>
2022-08-22 08:38:28 -0500

    proceso de la clase sueldo
    se comenzo a desarrollar la clase sueldo

--- src/Sueldos.java
+++ src/Sueldos.java
@@ -1,2 +1,15 @@
 public class Sueldos {
+
+    private float sueldo;
+    public void setSueldo(){
+        sueldo = 375.37f;
+    }
+
+    public void setSueldo(float nuevoSuedlo){
+        sueldo = nuevoSuedlo;
+    }
+
+    float getSuedlo (){
+        return sueldo;
+    }
 }
